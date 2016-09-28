557bd297e43ba3c81530e251939bf78f06881e4f
John <crictos@gmail.com>
2016-09-28 14:59:37 -0500

    se agrego una nueva activdad donde se crearan los formularios, ademas se adiciono una forma de agregar campos aun faltan detalles por corregir

--- app/src/main/java/com/codesuri/coodesuris/CrearEncuesta.java
+++ app/src/main/java/com/codesuri/coodesuris/CrearEncuesta.java
@@ -9,12 +9,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.view.ViewGroup.LayoutParams;
 
 public class CrearEncuesta extends AppCompatActivity implements View.OnClickListener{
-    LinearLayout svCampos;//permite contener los campos creados
+    LinearLayout llCampos;//permite contener los campos creados Verticalmente
+    RadioGroup rg;
+    Button btnOp;
     Button btnAddCampos;//permite crear campos
     Integer ContCamp = 0;//sirve para colocar un id diferente a los campos
 
@@ -23,37 +29,70 @@ public class CrearEncuesta extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_encuesta);
 
-        svCampos = (LinearLayout)findViewById(R.id.svCampos);
+        llCampos = (LinearLayout)findViewById(R.id.llCampos);
         btnAddCampos = (Button)findViewById(R.id.btnAddCampo);
-
         //comienza el escucha
         btnAddCampos.setOnClickListener(this);
 
-
-           /* String cadena = extra.getString("Cadena");
-            TextView valueTV = new TextView(this);
-            valueTV.setText(cadena);
-            valueTV.setId(5);
-            valueTV.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
-
-            ((LinearLayout) linearLayout).addView(valueTV);*/
-
-
     }
 
-    @Override
+   @Override
     public void onClick(View v) {
         switch(v.getId()){
-            //crea los campos
+            //crea los campos donde iran las preguntas del formulario
             case R.id.btnAddCampo:
                 EditText Campo = new EditText(this);
                 Campo.setId(ContCamp);
                 Campo.setHint("Ingrese una pregunta:");
                 Campo.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
-                svCampos.addView(Campo);
+                llCampos.addView(Campo);
+
+                btnOp = new Button(this);
+                btnOp.setText("Add");
+                btnOp.setId(ContCamp);
+                btnOp.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+                btnOp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        CrearRB();
+                    }
+                });
+                llCampos.addView(btnOp);
+
+
+
+                rg = new RadioGroup(this);
+                rg.setOrientation(LinearLayout.HORIZONTAL);
+                rg.setId(ContCamp);
+                rg.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+                llCampos.addView(rg);
+
+
+                CrearRB();
+
                 ContCamp++;
+
+                ////////////////////////////////////////////////////////////////////////////////////////////
+
+
             break;
 
         }
+
+    }
+
+    public void CrearRB(){
+
+        RadioButton rbOp = new RadioButton(this);
+        rbOp.setId(ContCamp);
+        rg.addView(rbOp);
+
+        EditText etOp = new EditText(this);
+        etOp.setHint("Opción");
+        etOp.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+
+        rg.addView(etOp);
+
+
     }
 }
